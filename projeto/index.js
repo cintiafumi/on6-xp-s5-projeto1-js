17e01ba73c10d5a6fce3c4f5ee55c9aa0469a83f
Cintia Fumi <cintiafumi@gmail.com>
2020-09-09 20:10:16 -0300

    Adicionado readline

--- projeto/index.js
+++ projeto/index.js
@@ -3,3 +3,9 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
+
+const readline = require('readline-sync')
+
+const resposta = readline.question('Quantos anos você tem? ')
+
+console.log(resposta)
